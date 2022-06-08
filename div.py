c1f86d5169c9c6ae2160fc402cc38ccc8226c242
ikuawamuti <105683762+ikuawamuti@users.noreply.github.com>
2022-06-08 10:25:03 +0300

    Add files via upload

---
+++ div.py
@@ -0,0 +1,19 @@
+
+
+
+
+def print_nums(x):
+    for num in range(x):
+        if num > 6:
+            print("less than 6")
+        if num == 6:
+            print("equal to 6")
+        if num < 10:
+            print("greater than 10")
+    
+        
+        
+print_nums(20)
+
+
+
